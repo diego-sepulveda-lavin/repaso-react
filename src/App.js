6339305e65b51ffe9b35995e9d1a07e44b3b480c
Diego Sepulveda <diegosl@hotmail.cl>
2021-04-07 13:51:28 -0400

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
-import Padre from "./components/Padre"
+import React, { useEffect, useState } from "react";
 
-function App() {
-	return <Padre />
-}
+const App = () => {
+	const [nombre, setNombre] = useState("Juan");
+	const [estado, setEstado] = useState(false)
+
+	useEffect(() => {
+		console.log('Yo solo me ejecuto en el montaje') //ComponentDidMount
+	}, []);
+
+	useEffect(() => {
+		console.log('Yo solo me ejecuto en el montaje o cuando nombre se actualiza') //ComponentDidMount y ComponentDidUpdate
+	}, [nombre]);
+
+	useEffect(() => {
+		console.log('Yo me ejecuto en el montaje y siempre') // ComponentDidMount y Siempre
+	}, );
+
+	const handleName = e => {
+		setNombre(e.target.value);
+	};
+
+	return (
+		<div>
+			<h1>Hola mi nombre es {nombre} y el estado es: {estado?"Verdadero":"Falso"}</h1>
+			<input onChange={handleName} />
+			<button onClick={()=> setEstado(prevState => !prevState)}>Cambiar estado</button>
+		</div>
+	);
+};
 
 export default App;
