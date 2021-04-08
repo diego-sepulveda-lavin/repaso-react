36fbad0c4bda9c64a7d466aadf887e7f0c1cd6ac
Diego Sepulveda <diegosl@hotmail.cl>
2021-04-08 12:49:34 -0400

    estados complejos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+	const [estado, setEstado] = useState({
+		activo: false,
+		nombre: "",
+		favoritos: []
+	});
 
-	const [estado, setEstado] = useState(null)
 
-	useEffect(()=>{
-		fetch('https://swapi.dev/api/people/1/')
-		.then(response => response.json())
-		.then(data => {
-			console.log(data)
-			setEstado(data)
-		})
-		.catch(error => console.log(error))
-	},[])
+	const [activo, setActivo] = useState(false)
+	const [nombre, setNombre] = useState("")
+	const [favoritos, setFavoritos] = useState([])
+
+
+
+	const handleName = e => {
+		setEstado(prevState => ({ ...prevState, nombre: e.target.value }));
+		setNombre(e.target.value)
+	};
+
+	const handleActivo = () => {
+		setEstado(prevState => ({ ...prevState, activo: !prevState.activo }));
+	};
+
+	const handleFav = () =>{
+		let newFav = {id:1, nombre:"Luke"}
+		let favcopy = [...estado.favoritos]
+		favcopy.push(newFav)
+		setEstado(prevState=>({...prevState, favoritos:favcopy}))
+	}
 
 	return (
 		<div>
-			<h1>Hola Mundo, mi nombre es {!!estado? estado.name : '...cargando'}</h1>
+			<h1>Hola Mundo</h1>
+			<input onChange={handleName} />
+			<button onClick={handleActivo}>Cambiar activo</button>
+			<button onClick={handleFav}>Agregar a Fav</button>
 		</div>
 	);
 };
+
+
 
 export default App;
